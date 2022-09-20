c93d2fd14e2a1b3cf0d85ff4cf44daa275044537
Chris Walker <thatamazingweb@gmail.com>
2022-09-20 20:18:09 +0100

    Add gmilinks build; linting; no-cache testing

--- run.sh
+++ run.sh
@@ -1,43 +1,53 @@
 #!/usr/bin/env bash
 
+lint() {
+  echo "Running linters..."
+  golangci-lint run ./...
+}
+
 build() {
-    build-gmiget
-    build-gmifmt
+  build-gmiget
+  build-gmifmt
+  build-gmilinks
 }
 
 build-gmiget() {
-    echo "Building gmiget..."
-    go build -o bin/gmiget cmd/gmiget/main.go
+  echo "Building gmiget..."
+  go build -o bin/gmiget cmd/gmiget/main.go
 }
 
 build-gmifmt() {
-    echo "Building gmifmt..."
-    go build -o bin/gmifmt cmd/gmifmt/main.go
+  echo "Building gmifmt..."
+  go build -o bin/gmifmt cmd/gmifmt/main.go
+}
+
+build-gmilinks() {
+  echo "Building gmilinks..."
+  go build -o bin/gmilinks cmd/gmilinks/main.go
 }
 
 test() {
-    echo "Running all tests..."
-    go test -cover ./...
+  echo "Running all tests..."
+  go test -test.count=1 -cover ./...
 }
 
 help() {
-    printf "%s <task> <args>\n" "$0"
-    printf "Tasks:\n"
-    compgen -A function | cat -n
+  printf "%s <task> <args>\n" "$0"
+  printf "Tasks:\n"
+  compgen -A function | cat -n
 }
 
 die() {
-    printf "%s\n" "$0"
-    exit 1
+  printf "%s\n" "$@"
+  exit 1
 }
 
 action="$1"
 case $action in
-    build|build-gmiget|build-gmifmt|test|help)
-        "$@"
+  lint | build | build-gmiget | build-gmifmt | build-gmilinks | build-gmisrv | test | help)
+    "$@"
     ;;
-    *)
-        die "invalid action '${action}"
+  *)
+    die "invalid action '${action}'"
     ;;
 esac
-
